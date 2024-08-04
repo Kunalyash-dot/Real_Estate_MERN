13608dd303b7293d3612dd8ee72eafe0c2e9324e
Kunalyash-dot <kunalcivil20@gmail.com>
2024-08-04 15:56:28 +0530

    coneected to mongoDB using Mongoose

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
 
 const app = express();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connect to MongoDB")
+}).catch((error)=>{
+    console.log(`Not connected Error ${error}`)
+})
+
 
 app.listen(3000,()=>{
     console.log("Server runing at port 3000!")
