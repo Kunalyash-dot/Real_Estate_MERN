ecfa8266d408ee435c6c171111dae232ddf0ebdf
Kunalyash-dot <kunalcivil20@gmail.com>
2024-09-17 21:10:25 +0530

    Add contact landlord functionality to the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -12,17 +12,21 @@ import {
   FaBed,
   FaChair,
   FaMapMarkedAlt,
-  FaMapMarkerAlt,
   FaParking,
   FaShare,
 } from 'react-icons/fa';
+import {useSelector} from 'react-redux'
+import Contact from "../components/Contact";
 
 function Listing() {
   const [copied, setCopied] = useState(false);
     const [listing,setListing] =useState(null);
     const [loading,setLoading] = useState(false);
     const [error,setError]= useState(false);
+    const [contact, setContact] = useState(false); 
     const params = useParams();
+    const {currentUser} = useSelector((state) => state.user);
+
     useEffect(()=>{
       const fetchListing = async ()=>{
         try {
@@ -116,6 +120,12 @@ function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
+              {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing}/>}
            </div>
        </div>      
       )}
