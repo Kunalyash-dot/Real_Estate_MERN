d6b708fcb218ffbdb762fe51a07ebe62df2fcdf2
Kunalyash-dot <129615876+Kunalyash-dot@users.noreply.github.com>
2025-07-10 10:09:55 +0530

    moved backend package folder to root

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config()
 
@@ -12,14 +13,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
-mongoose.connect(process.env.MONGO).then(()=>{
+// console.log(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("connect to MongoDB")
 }).catch((error)=>{
     console.log(`Not connected Error ${error}`)
 })
 
-
+ const __dirname = path.resolve();
 
 app.listen(3000,()=>{
     console.log("Server runing at port 3000!")
@@ -29,6 +30,11 @@ app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing',listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
